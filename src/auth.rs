d5a10a51c00108678b26bf63c884aaf367272efd
Boiko Oleg <etot.mir.realen@gmail.com>
2026-09-08 04:22:01 +0900

    refactor: make auth claims cloneable

--- src/auth.rs
+++ src/auth.rs
@@ -3,7 +3,7 @@ use serde::{Deserialize, Serialize};
 use anyhow::{anyhow, Result};
 use crate::config::AuthConfig;
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Claims {
     pub sub: String,
     pub exp: usize,
